c1917a80729395d9cbc0e3aa2fdbe60d471424a5
benshirom <57137329+benshirom@users.noreply.github.com>
2022-12-11 14:40:54 +0200

    add populate orders

--- api/controllers/restaurantControll.js
+++ api/controllers/restaurantControll.js
@@ -104,7 +104,7 @@ exports.RestaurantCtrl = {
   getRestaurant: async (req, res) => {
     try {
       let { resId } = req.params
-      let data = await RestaurantModel.findOne({ _id: resId }).populate({path: 'menu',model: 'itemmenus'});
+      let data = await RestaurantModel.findOne({ _id: resId }).populate({path: 'menu',model: 'itemmenus'}).populate({path: 'orders',model: 'orders'});
       res.json(data);
     }
     catch (err) {
