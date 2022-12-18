3b076e9a34752d500939213b26c58319a9fee99d
benshirom <57137329+benshirom@users.noreply.github.com>
2022-12-18 21:14:58 +0200

    v

--- api/controllers/restaurantControll.js
+++ api/controllers/restaurantControll.js
@@ -169,6 +169,7 @@ exports.RestaurantCtrl = {
 
   },
   setCanvas: async (req, res) => {
+    console.log(req.body)
     let validBody = validateTablesCanvas(req.body);
     if (validBody.error) {
       return res.status(400).json({ msg: "Need to send body" });
