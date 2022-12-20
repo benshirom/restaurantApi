9b8b03ac206858b97de1ad61e1d3c0306e41a3cc
benshirom <57137329+benshirom@users.noreply.github.com>
2022-12-20 16:00:10 +0200

    fix validateEditItemMenu

--- api/validation/itemMenuValidation.js
+++ api/validation/itemMenuValidation.js
@@ -21,8 +21,8 @@ exports.validateEditItemMenu = (_reqBody) => {
         info: Joi.string().min(3).max(100).allow(null, ""),
         img: Joi.string().min(2).max(999).allow(null, ""),
         video: Joi.string().min(2).max(999).allow(null, ""),
-        price: Joi.number(),
-        calories: Joi.number(),
+        price: Joi.number().min(0).required(),
+        calories: Joi.number().allow(null, 0),
         category:Joi.string().min(2).max(20).required(),
         subCategory: Joi.string().min(2).max(20).allow(null, "")
        
