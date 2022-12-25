5ac2508d4fbf7e597d462a93a4be65106edf4e3a
benshirom <57137329+benshirom@users.noreply.github.com>
2022-12-25 19:14:04 +0200

    d

--- api/routes/tables.js
+++ api/routes/tables.js
@@ -11,7 +11,7 @@ router.patch("/create/:restId" ,authManager,TableCtrl.createNewTable)
 router.delete("/remove/:restId/:tableId" ,authManager,TableCtrl.removeTableFromRestaurant) 
 router.patch("/edit/:editTableId", authManager, TableCtrl.editTable)
 router.patch("/editOrderID/:editTableId/:orderID", authManager, TableCtrl.editOrderID)
-router.patch("/location/:editTableId", authManager, TableCtrl.editLocation)
+// router.patch("/location/:editTableId", authManager, TableCtrl.editLocation)
 // router.patch("/tableOwenr/:editTableId/:orderId", authManager, TableCtrl.editTableOwenr)
 
 
