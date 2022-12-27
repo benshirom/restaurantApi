874460de3e417ebcd801b5f46233e1c116d6889e
benshirom <57137329+benshirom@users.noreply.github.com>
2022-12-27 18:28:22 +0200

    fix editOrderID

--- api/routes/tables.js
+++ api/routes/tables.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.patch("/create/:restId" ,authManager,TableCtrl.createNewTable) 
 router.delete("/remove/:restId/:tableId" ,authManager,TableCtrl.removeTableFromRestaurant) 
 router.patch("/edit/:editTableId", authManager, TableCtrl.editTable)
-router.patch("/editOrderID/:editTableId/:orderID", authManager, TableCtrl.editOrderID)
+router.patch("/editOrderID/:editTableId/:orderID", authWorker, TableCtrl.editOrderID)
 // router.patch("/location/:editTableId", authManager, TableCtrl.editLocation)
 // router.patch("/tableOwenr/:editTableId/:orderId", authManager, TableCtrl.editTableOwenr)
 
