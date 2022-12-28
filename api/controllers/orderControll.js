c7347112bc223553b2b25ac477f116eef8245d71
benshirom <57137329+benshirom@users.noreply.github.com>
2022-12-28 15:22:46 +0200

    v

--- api/controllers/orderControll.js
+++ api/controllers/orderControll.js
@@ -185,7 +185,7 @@ exports.OrderCtrl = {
       let item = new itemOrderModel(req.body);
       item.itemMenuId=itemMenuId
       await item.save()
-      let order = await orderModel.updateOne({ _id: orderId },{ $push: { 'orderItems': itemMenuId } });
+      let order = await orderModel.updateOne({ _id: orderId },{ $push: { 'orderItems': item._id } });
       res.json(order);
 
     } catch (err) {
