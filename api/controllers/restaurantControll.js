c8c4f8d0115c0e8d42c6a2da5e9cd46596ab1d43
benshirom <57137329+benshirom@users.noreply.github.com>
2023-01-09 17:35:28 +0200

    v

--- api/controllers/restaurantControll.js
+++ api/controllers/restaurantControll.js
@@ -14,7 +14,7 @@ exports.RestaurantCtrl = {
   createRestaurant: async (req, res) => {
     let validBody = validateRestaurant(req.body);
 console.log(req.body);
-    if (validBody.error) return res.status(400).json(validBody.error.details);
+    if (validBody.error) return res.status(400).json(validBody.error.details,req.body);
 
     try {
       let userInfo = await UserModel.findOne({ _id: req.tokenData._id });
