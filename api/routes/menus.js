e563c5e4a1c296d522e6ac9aff3c6bc26c07a756
benshirom <57137329+benshirom@users.noreply.github.com>
2023-02-08 10:37:29 +0200

    f

--- api/routes/menus.js
+++ api/routes/menus.js
@@ -5,9 +5,9 @@ const { auth, authAdmin, authWorker, authManager, authWaiter, authChef } = requi
 const router = express.Router();
 
 router.get("/:restId", MenuCtrl.getMenu)
-router.post("/create/:restId", authChef, MenuCtrl.createItemMenu)
-router.patch("/remove/:restId/:itemId", authChef, MenuCtrl.removeItemMenu)
-router.patch("/edit/:editItemId", authChef, MenuCtrl.editItemMenu)
+router.post("/create/:restId", authManager, MenuCtrl.createItemMenu)
+router.patch("/remove/:restId/:itemId", authManager, MenuCtrl.removeItemMenu)
+router.patch("/edit/:editItemId", authManager, MenuCtrl.editItemMenu)
 // router.patch("/editcategory/:editItemId", authManager, MenuCtrl.editCategoryItemMenu)
 // router.patch("/editsubcategory/:editItemId", authManager, MenuCtrl.editSubcategoryItemMenu)
 
